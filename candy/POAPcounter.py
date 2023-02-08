ca830c47c75ccef51b34efe0925dcf64deb6141a
Glake <0xglake@gmail.com>
2023-02-09 03:49:26 +1100

    update to new NFT and experiement with RPCs

--- candy/POAPcounter.py
+++ candy/POAPcounter.py
@@ -6,11 +6,11 @@ result = ""
 while count < 333:
   current = '''
       "{}": {{
-      "name": "Drift Summer Camp POAP",
-      "image_hash": "6e9e70498bd3663744dd92f66bcf384862c5892bc2fa856665bcfa9b503beb8b",
-      "image_link": "https://arweave.net/p5jb7696kiHaE7k2yZ0vRO6XhlpUioqWPTHzRlmJmRQ?ext=png",
-      "metadata_hash": "8f6592b14c7a0be3bcc0c6a03dc6567d8ba5a69fdc3ccbced45ed6a2ab3eb389",
-      "metadata_link": "https://arweave.net/KvsO0AVo2Tjy-dRmVYfZ-43Or2XTkY2mIqN4fFtCIdY",
+      "name": "Summer Camp Shark Tank POAP",
+      "image_hash": "dd7db7b28dff70ec2d94c4bb5120124dfdc32c955cafca6a6d4599c493ea642f",
+      "image_link": "https://arweave.net/FNieJLTDjKHjTxzIHukBuYpXv3vUuY23FSQZ0FAAfVQ?ext=png",
+      "metadata_hash": "37dc608500fca0b869a69e19ff78acbf542d1049d623f8c46724df47f0cab247",
+      "metadata_link": "https://arweave.net/3brze0si1CQb6ZKql5MRQ-juoB4OagcQbIHESKNOb1Y",
       "onChain": false
     }},'''.format(count)
 
